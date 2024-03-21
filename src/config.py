664edd93ada002d3bac5590cc29262f56067d74e
bastienlc <78959054+bastienlc@users.noreply.github.com>
2024-03-21 13:22:20 +0100

    Add seed_everything for reproducibility

--- src/config.py
+++ src/config.py
@@ -1,4 +1,8 @@
 import os
+import random
+
+import numpy as np
+import torch
 
 
 class CONFIG:
@@ -12,3 +16,19 @@ class CONFIG:
 
     cols_annotation = ["GENDER", "LYMPH_COUNT", "age"]
     col_label = "LABEL"
+
+    SEED = 42
+
+
+def seed_everything(seed: int):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+
+
+seed_everything(CONFIG.SEED)
